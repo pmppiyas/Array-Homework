e60fadf73b216e4b56b04568a1e08dbb1e0df252
Prince Mahmud Piyas <princemahmudpiyas@gmail.com>
2024-08-31 15:03:48 +0600

    Task3 Added

---
+++ task3.js
@@ -0,0 +1,6 @@
+let books = ["Bangla", "English", "Math", "Arabic", "Scince"]
+
+if (books.includes('Java Script')) {
+  console.log('Java Script are avaiable')
+}
+else( console.log('Java Script are not avaiable'))
